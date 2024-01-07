6c1a6c2ba449ec6eafbff6a46aa5b8f9b6318dad
hannahkamillia <hannahhamidi132@gmail.com>
2024-01-07 15:51:53 +0800

    initial

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -105,10 +105,10 @@ Vendor CSS Files
       <div class="carousel-inner" role="listbox">
 
         <!-- Slide 1 -->
-        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide1.jpg)">
           <div class="container">
-            <h2>Welcome to <span>Serenity Sphere</span></h2>
-            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel.</p>
+          <h2>Welcome to <span>Serenity Sphere</span></h2>
+            <p>Breathe. Believe. Be well. Start your journey to serenity at Serenity Sphere.</p>
             <a href="#about" class="btn-get-started scrollto">Read More</a>
           </div>
         </div>
