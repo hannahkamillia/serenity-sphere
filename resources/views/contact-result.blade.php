a7edc08512ab42863ae514c7af6687aee881ceb8
Suraya Aini <surayaa0103@gmail.com>
2024-01-15 18:24:40 +0800

    initial

--- resources/views/contact-result.blade.php
+++ resources/views/contact-result.blade.php
@@ -9,7 +9,6 @@
         <br><br><br><br>
         <h2>Contact</h2>
 
-        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
       </div>
 
     </div>
