9529fae4061b1ac4f73d4f8e7d80e02e234735f5
Naylee0913 <nananaylee@gmail.com>
2024-01-16 18:50:00 +0800

    init

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
-<a href="/">
-    <img src="assets/img/serenity sphere logo.png" alt="Logo" width="10%" height="10%">
+<a href="/" style="text-align: center">
+    <img src="assets/img/serenity sphere logo.png" alt="Logo"  width="10%" height="10%">
 </a>
--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,3 +1,3 @@
-<a href="/">
+<a href="/" style="text-align: center">
     <img src="assets/img/serenity sphere logo.png" alt="Logo"  width="10%" height="10%">
 </a>
