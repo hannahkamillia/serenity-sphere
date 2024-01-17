dc5d4b025741d6d7e69504de81f9e09f1f2bbc9f
Suraya Aini <surayaa0103@gmail.com>
2024-01-17 10:53:48 +0800

    initial

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -12,57 +12,56 @@
           <p>At SerenitySphere, we believe in the transformative power of well-being. In a world filled with constant demands and stresses, we recognize the need for a sanctuary that fosters healing and tranquility. SerenitySphere is more than just a therapy website – it's a refuge where individuals embark on a journey towards mental, emotional, and spiritual well-being.</p>
         </div>
 
+        <br><br>
+
         <div class="row">
           <div class="col-lg-6" data-aos="fade-right">
             <img src="assets/img/aboutus1.jpg" class="img-fluid" alt="">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
-            <h3>Your Sanctuary for Healing.</h3>
+            <h3>Your Sanctuary for Healing.</h3><br><br>
             <p class="fst-italic">
             Welcome to "Your Sanctuary for Healing," where we understand that the journey to mental well-being is unique for each individual. In this safe haven, we offer a compassionate space where healing is nurtured and celebrated.
-             Our platform is designed with your emotional wellness in mind, providing a refuge from the challenges life may present. 
-            </p>
+             Our platform is designed with your emotional wellness in mind, providing a refuge from the challenges life may present.
+            </p><br>
             <ul>
-              <li><i class="bi bi-check-circle"></i> Founded in 2023 in Kuala Lumpur, Malaysia by a group of IT students.</li>
-              <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+              <li><i class="bi bi-check-circle"></i> Founded in 2023 in Kuala Lumpur, Malaysia by a group of IIUM IT students.</li>
+              <li><i class="bi bi-check-circle"></i> We value the role of mental health and advocate for more readily available resources.</li>
+              <li><i class="bi bi-check-circle"></i> Currently located in the city centre, but the future holds for more Serenity Sphere branches across the country.</li>
             </ul>
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
+                فَإِنَّ مَعَ ٱلْعُسْرِ يُسْرًا ٥
+                إِنَّ مَعَ ٱلْعُسْرِ يُسْرًۭا ٦
+                <br><br>“For indeed, with hardship [will be] ease. Surely with ˹that˺ hardship comes ˹more˺ ease.”  --Surah al-Inshirah
             </p>
           </div>
         </div>
 
     </div>
     </section><!-- End About Us Section -->
-    
+
     <!-- ======= Gallery Section ======= -->
     <section id="gallery" class="gallery">
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
           <h2>Gallery</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
 
         <div class="gallery-slider swiper">
           <div class="swiper-wrapper align-items-center">
-            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-1.jpg"><img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-2.jpg"><img src="assets/img/gallery/gallery-2.jpg" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-3.jpg"><img src="assets/img/gallery/gallery-3.jpg" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-4.jpg"><img src="assets/img/gallery/gallery-4.jpg" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-5.jpg"><img src="assets/img/gallery/gallery-5.jpg" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-6.jpg"><img src="assets/img/gallery/gallery-6.jpg" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-7.jpg"><img src="assets/img/gallery/gallery-7.jpg" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-8.jpg"><img src="assets/img/gallery/gallery-8.jpg" class="img-fluid" alt=""></a></div>
+            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-1.jpeg"><img src="assets/img/gallery/gallery-1.jpeg" class="img-fluid" alt=""></a></div>
+            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-2.jpeg"><img src="assets/img/gallery/gallery-2.jpeg" class="img-fluid" alt=""></a></div>
+            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-3.jpeg"><img src="assets/img/gallery/gallery-3.jpeg" class="img-fluid" alt=""></a></div>
+            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-4.png"><img src="assets/img/gallery/gallery-4.png" class="img-fluid" alt=""></a></div>
+            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-5.jpeg"><img src="assets/img/gallery/gallery-5.jpeg" class="img-fluid" alt=""></a></div>
+            <div class="swiper-slide"><a class="gallery-lightbox" href="assets/img/gallery/gallery-6.jpeg"><img src="assets/img/gallery/gallery-6.jpeg" class="img-fluid" alt=""></a></div>
           </div>
           <div class="swiper-pagination"></div>
         </div>
 
       </div>
-    </section><!-- End Gallery Section -->        
+    </section><!-- End Gallery Section -->
 
     <section id="joinus" class="joinus section-bg">
       <div class="container" data-aos="fade-up">
@@ -78,7 +77,7 @@ Thank you for choosing SerenitySphere – where your journey to serenity begins.
 
         <form action="Join" method="post" role="form" class="php-email-form" data-aos="fade-up" data-aos-delay="100">
           @csrf
-        
+
           <div class="row">
             <div class="col-md-4 form-group">
               <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
@@ -100,7 +99,7 @@ Thank you for choosing SerenitySphere – where your journey to serenity begins.
                 <option value="Position 4">Human Resources</option>
               </select>
             </div>
-            
+
           </div>
 
           <div class="form-group mt-3">
